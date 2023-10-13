938d1099d206f669749c2efa7f47b042731e6021
Kyle Taylor Lange <dakatsu@gmail.com>
2023-10-12 20:10:03 -0400

    Build SASS styles

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ from datetime import datetime
 # Deletes old files from distribution directories.
 def cleanFiles():
   print("Deleting old files.")
-  paths_to_delete = [os.path.join("dist", "src"), os.path.join("dist", "json")]
+  paths_to_delete = [os.path.join("dist", "src"), os.path.join("dist", "json"), os.path.join("dist", "css")]
 
   for rel_path in paths_to_delete:
     if (os.path.exists(rel_path)):
@@ -17,7 +17,7 @@ def cleanFiles():
         raise Exception("Unable to delete: " + rel_path)
 
 # Runs the TypeScript compiler.
-def runCompiler():
+def runTypeScriptCompiler():
   # Run the TypeScript compiler
   ts_compiler_path = shutil.which("tsc")
 
@@ -32,13 +32,27 @@ def runCompiler():
 
   print("Compilation finished successfully.")
 
+def buildStyles():
+    # Ensure Node Package Manager is installed.
+    print("Building SASS styles.")
+    npm_path = shutil.which("npm")
+    if type(npm_path) != str:
+      raise Exception("unable to find Node Package Manager")
+    
+    # Rebuild the SASS styles.
+    process_results = subprocess.run([npm_path, "run", "build-styles"])
+    if (process_results.returncode != 0):
+      raise Exception("error building SASS styles")
+    print("Styles built successfully.")
+
 if __name__ == "__main__":
   print("Starting build script.")
   start_time = datetime.now().timestamp()
 
   try:
     cleanFiles()
-    runCompiler()
+    runTypeScriptCompiler()
+    buildStyles()
     elapsed_time = datetime.now().timestamp() - start_time
     print("Build script completed successfully in " + format(elapsed_time, ".3f") + "s.")
   except Exception as ex:
