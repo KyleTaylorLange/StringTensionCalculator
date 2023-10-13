162cd8335fa97d96eac54231b753687c77f828a4
Nicholas LaMothe <lamothe.dev@gmail.com>
2023-10-12 18:01:31 -0700

    very small changes to scripts

--- prepare.py
+++ prepare.py
@@ -3,6 +3,7 @@ import subprocess
 import shutil
 from datetime import datetime
 
+
 if __name__ == "__main__":
   print("Starting prepare script.")
   start_time = datetime.now().timestamp()
@@ -10,14 +11,18 @@ if __name__ == "__main__":
   try:
     # Find NPM's path.
     npm_path = shutil.which("npm")
+
     if type(npm_path) != str:
       raise Exception("unable to find Node Package Manager")
     
     # Install both SASS and Typescript via NPM.
     process_results = subprocess.run([npm_path, "install", "sass"])
+
     if (process_results.returncode != 0):
       raise Exception("error installing SASS via NPM")
+    
     process_results = subprocess.run([npm_path, "install", "typescript"])
+
     if (process_results.returncode != 0):
       raise Exception("error installing TypeScript via NPM")
 
