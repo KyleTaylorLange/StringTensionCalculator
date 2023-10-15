b7a3a413ed02d9ca3b51ac94b46e3336772d8fd2
Kyle Taylor Lange <dakatsu@gmail.com>
2023-10-15 11:40:48 -0400

    Temp remove getJson call.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { Utilities } from './dist/src/static/Utilities.js'
 import { Main } from './dist/src/Main.js'
 
-let jsonResult = Utilities.getJson('DAddario')
+//let jsonResult = Utilities.getJson('DAddario')
 
 /**
  * On load, feed promise data from jsonResult into Main() to be used throughout the program.
